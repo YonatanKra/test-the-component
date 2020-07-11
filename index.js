88fc7dbff0f61e432a61fcc4b2b5f99dd8e18c43
Yonatan <kra.yonatan@gmail.com>
2020-07-11 21:07:49 +0300

    Jest is working with lit-element
    Notice the jest config
    Also notice the @babel/polyfill involved

---
+++ index.js
@@ -0,0 +1,1 @@
+import '@yonatankra/test-component';
---
+++ index.spec.js
@@ -0,0 +1,8 @@
+import '@babel/polyfill';
+import './index';
+
+describe('index', () => {
+    it('should do', () => {
+        expect(true).toBeTruthy();
+    });
+});
